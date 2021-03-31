f305ae198724e976246860f299233b9856844775
ahemd el-rewaidy <64925287+elreweidy@users.noreply.github.com>
2021-03-31 15:20:49 +0200

    json list updated

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import cv2
 import imutils
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
 import utils
 
 # Reading images
-main_paper = cv2.imread("OMR4Main.png")
+main_paper = cv2.imread("c:/users/ahmed/Desktop/OMR4.jpg")
 width, height, channels = main_paper.shape  # extract image shape
-
 
 if main_paper is None:  # checking if the images is valid to be read or Not.
     sys.exit("couldn't read this images")
@@ -24,12 +24,12 @@ main_paper_rgb = cv2.cvtColor(main_paper, cv2.COLOR_BGR2RGB)
 """ the previous line actually convert the BGR image to RGB image first because that is
  what is used in matplotlib and if we don't do it, it will mix things up eventually"""
 
-
-main_paper_gray = cv2.cvtColor(main_paper_rgb, cv2.COLOR_RGB2GRAY)# Here we convert our RGB to gray image.
-""" So, if we plot the imges now we will see it's converted to grayscale image.
+main_paper_gray = cv2.cvtColor(main_paper_rgb, cv2.COLOR_RGB2GRAY)  # Here we convert our RGB to gray image.
+""" So, if we plot the images now we will see it's converted to grayscale image.
     to do that us the code below again:"""
 
-main_paper_blur = cv2.GaussianBlur(main_paper_gray, (5, 5), 1)# Making the image blur (necessary for edge detection below).
+main_paper_blur = cv2.GaussianBlur(main_paper_gray, (5, 5),
+                                   1)  # Making the image blur (necessary for edge detection below).
 
 # ---------------EDGE DETECTION AND CONTOURS------------------------------
 
@@ -44,7 +44,6 @@ cv2.drawContours(main_paper_copy, main_paper_contours, -1, (0, 0, 255), 1)
 rectangles = utils.find_sorted_rectangles(contours=main_paper_contours)
 
 question_box = utils.detect_corners(rectangles[0])
-# Mark corners of the question box in the main_paper
 cv2.drawContours(main_paper, question_box, -1, (0, 0, 255), 10)
 
 # ----------------------------- RESHAPING THE CORNERS -----------------------------
@@ -63,7 +62,6 @@ dim = (width, height)
 questions_image = cv2.resize(questions_image, dim, interpolation=cv2.INTER_AREA)
 h, w, c = questions_image.shape
 
-
 questions_image_copy = questions_image.copy()
 # ---------------------------------- REUSE ----------------------------------------
 questions_image_gray = cv2.cvtColor(questions_image_copy, cv2.COLOR_RGB2GRAY)
@@ -79,7 +77,7 @@ as a threshold to push value to black when exceeding it"""
 
 # the list of contours that correspond to questions
 questions_image_contours = cv2.findContours(questions_image_thresh, cv2.RETR_EXTERNAL,
-                       cv2.CHAIN_APPROX_SIMPLE)
+                                            cv2.CHAIN_APPROX_SIMPLE)
 questions_image_contours = imutils.grab_contours(questions_image_contours)
 
 questions_image_contours.sort(key=lambda x: utils.get_contour_precedence(x, main_paper.shape[1]))
@@ -96,7 +94,7 @@ for c in questions_image_contours:
     # have an aspect ratio approximately equal to 1
     if w >= 12 and h >= 12 and 0.8 <= ar <= 1.5:
         circles_contours.append(c)
-cv2.drawContours(questions_image_copy, circles_contours[50], -1, (0, 0, 255), 1)
+cv2.drawContours(questions_image, circles_contours, -1, (0, 0, 255), 1)
 print(len(circles_contours))
 
 """ 
@@ -108,7 +106,8 @@ print(len(circles_contours))
 
 # NOW we have circles_contours which has all 800 sorted contours
 # apply Otsu's threshold method to binarize the warped piece of paper
-bubbled_thresh = cv2.threshold(questions_image_gray, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]  # inverted values (search for non-zeros)
+bubbled_thresh = cv2.threshold(questions_image_gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[
+    1]  # inverted values (search for non-zeros)
 # we will loop on every contour and get its value
 bubbled = []
 for (j, c) in enumerate(circles_contours):
@@ -122,16 +121,29 @@ for (j, c) in enumerate(circles_contours):
     # bubble area
     mask = cv2.bitwise_and(bubbled_thresh, bubbled_thresh, mask=mask)
     total = cv2.countNonZero(mask)
-    is_bubbled = False
-    if(total > 100):
-        is_bubbled = True
+    is_bubbled = 0
+    if total > 100:
+        is_bubbled = 1
     bubbled.append(is_bubbled)
-    print("index: ", j, "total:", total, "is bubbled: ", is_bubbled)
+    # print("index: {} total: {} bubbled?: {}".format(j, total, is_bubbled))
+
+
+# creating an answer dict
+
+def list_to_json():
+    answers_json = json.dumps(bubbled)
+    return answers_json
+
 
 # ------------------------------------------ plotting ----------------------------
-plt.figure("question table")
-plt.imshow(questions_image_copy, cmap="gray")  # plotting the image.
-plt.show()
-k = cv2.waitKey(0)  # preventing the images to automatically just disappear.
-if k == ord("s"):  # save the image if we pressed the letter s in out keyboard.
-    cv2.imwrite("OMR4.jpg", main_paper)
+# plt.figure("question table")
+# plt.imshow(questions_image, cmap="gray")  # plotting the image.
+# plt.show()
+# k = cv2.waitKey(0)  # preventing the images to automatically just disappear.
+# if k == ord("s"):  # save the image if we pressed the letter s in out keyboard.
+# cv2.imwrite("OMR4.jpg", main_paper)
+
+if __name__ == '__main__':
+    list_to_json()
+
+
